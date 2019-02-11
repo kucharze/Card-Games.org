408ab5c7b02a513203802e6cc593d6c35151ef80
kucharze <kucharZ@duq.edu>
2019-02-10 20:01:48 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-var express = require('express');
-const path=require('path');
-//const ws=require('WebSocket');
-var app = express();
-var port = process.env.PORT || 3000;
+// HTTP Module for Creating Server and Serving Static Files Using Node.js
+// Static Files: HTML, CSS, JS, Images
+// Get Complete Source Code from Pabbly.com
 
-app.get('/',function(req,res){
-   //response.send("Welcome");
-    //response.sendFile('index.html');
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
-app.listen(port);
+http.createServer(function(req, res){
+    if(req.url === "/"){
+        fs.readFile("index.html", "UTF-8", function(err, html){
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(html);
+        });
+    }else if(req.url.match("\.css$")){
+        var cssPath = path.join(__dirname, '/', req.url);
+        var fileStream = fs.createReadStream(cssPath, "UTF-8");
+        res.writeHead(200, {"Content-Type": "text/css"});
+        fileStream.pipe(res);
+
+    }else if(req.url.match("\.png$")){
+        var imagePath = path.join(__dirname, '/', req.url);
+        var fileStream = fs.createReadStream(imagePath);
+        res.writeHead(200, {"Content-Type": "image/png"});
+        fileStream.pipe(res);
+    }else{
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end("No Page Found");
+    }
+
+}).listen(3000);
